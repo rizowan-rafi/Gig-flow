71a4ee6bf6292f388aab250b62fa7e0ac519f8ba
isagi ego <isagiego299@gmail.com>
2025-01-26 07:53:02 +0600

    Admin route has been completed

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -63,7 +63,6 @@ const AuthProvider = ({ children }) => {
                 const userInfo = { email: user.email };
                 axiosPublic.post("/jwt", userInfo).then((res) => {
                     if (res?.data?.token) {
-                        // console.log(res.data?.token);
                         localStorage.setItem("token", res.data.token);
                     }
                 });
